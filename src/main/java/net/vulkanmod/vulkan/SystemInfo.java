225f645d04a36c5ca8ccab8baf429af3c912040f
whbex <66699673+whitebelyash@users.noreply.github.com>
2025-01-11 23:58:06 +0400

    [HACK] SystemInfo: do not crash on class init failure

--- src/main/java/net/vulkanmod/vulkan/SystemInfo.java
+++ src/main/java/net/vulkanmod/vulkan/SystemInfo.java
@@ -1,12 +1,21 @@
 package net.vulkanmod.vulkan;
 
+import net.vulkanmod.Initializer;
 import oshi.hardware.CentralProcessor;
 
 public class SystemInfo {
     public static final String cpuInfo;
 
     static {
-        CentralProcessor centralProcessor = new oshi.SystemInfo().getHardware().getProcessor();
-        cpuInfo = String.format("%s", centralProcessor.getProcessorIdentifier().getName()).replaceAll("\\s+", " ");
+        CentralProcessor centralProcessor = null;
+        // Opening F3 crashes the game on one specific platform, this hack fixes it
+        try {
+            centralProcessor = new oshi.SystemInfo().getHardware().getProcessor();
+        } catch (NoClassDefFoundError e){
+            Initializer.LOGGER.warn("Failed to initialize OSHI class, no cpu info will be available");
+        }
+        cpuInfo = centralProcessor != null ?
+                String.format("%s", centralProcessor.getProcessorIdentifier().getName()).replaceAll("\\s+", " ") :
+                "Unknown";
     }
 }
